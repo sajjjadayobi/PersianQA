8d3cd51cd256668856538c678769d6b1b4fc5f26
Sajjad Ayobi <43270899+sajjjadayobi@users.noreply.github.com>
2021-04-12 17:06:47 +0430

    Update reading.py

--- dataset/reading.py
+++ dataset/reading.py
@@ -1,10 +1,10 @@
-from collections import OrderedDict
 from pathlib import Path
 
 
-def read_ds(path):
+def read_qa(path):
     """
     this reads dataset from json files like SQuAD v2
+    you can use this function for loading train and test file
     """
     path = Path(path)
     ds = []
@@ -28,31 +28,19 @@ def read_ds(path):
                       "answer_start": answer_starts,
                       "text": answers},})
     return ds
-  
-
-def convert2order(ds):
-    """
-    convert ds object to an OrderDict 
-    """
-    return OrderedDict([('answers', [i['answers'] for i in ds]), 
-                      ('context', [i['context'] for i in ds]), 
-                      ('question', [i['question'] for i in ds])])
 
 
-def combine(ds1, ds2):
-    """
-    for combinig PersianQA with SQuAD v2
-    """
-    return OrderedDict([('answers', ds1['answers']+ds2['answers']), 
-                      ('context', ds1['context']+ds2['context']), 
-                      ('question', ds1['question']+ds2['question'])])
 
   
 # example  
 if __name__ == "__main__":
-    train_ds = convert2order(read_ds('PersianQA-train.json'))
-    valid_ds = convert2order(read_ds('PersianQA-valid.json'))
+    train_ds = read_qa('PersianQA-train.json')
+    valid_ds = read_qa('PersianQA-valid.json')
     
-    # if you want use PersianQA + SQuAD v2
-    squad_train_ds = convert2order(read_ds('train-v2.0.json'))
-    train_ds = combine(squad_train_ds, train_ds)
+    # Example
+    print(train_ds[0])
+    # >>> {'title': 'شرکت فولاد مبارکه اصفهان',
+    # >>> 'context': 'شرکت فولاد مبارکۀ اصفهان، بزرگ ترین واحد صنعتی خصوصی در ایران و بزرگ ترین مجتمع تولید فولاد در کشور ایران است، که در شرق شهر مبارکه قرار دارد. فولاد مبارکه هم اکنون محرک بسیاری از صنایع بالادستی و پایین دستی است. فولاد مبارکه در ۱۱ دوره جایزۀ ملی تعالی سازمانی و ۶ دوره جایزۀ شرکت دانشی در کشور رتبۀ نخست را بدست آورده است و همچنین این شرکت در سال ۱۳۹۱ برای نخستین بار به عنوان تنها شرکت ایرانی با کسب امتیاز ۶۵۴ تندیس زرین جایزۀ ملی تعالی سازمانی را از آن خود کند. شرکت فولاد مبارکۀ اصفهان در ۲۳ دی ماه ۱۳۷۱ احداث شد و اکنون بزرگ ترین واحدهای صنعتی و بزرگترین مجتمع تولید فولاد در ایران است. این شرکت در زمینی به مساحت ۳۵ کیلومتر مربع در نزدیکی شهر مبارکه و در ۷۵ کیلومتری جنوب غربی شهر اصفهان واقع شده است. مصرف آب این کارخانه در کمترین میزان خود، ۱٫۵٪ از دبی زاینده رود برابر سالانه ۲۳ میلیون متر مکعب در سال است و خود یکی از عوامل کم آبی زاینده رود شناخته می شود.',
+    # >>> 'question': 'شرکت فولاد مبارکه در کجا واقع شده است',
+    # >>> 'id': 1,
+    # >>> 'answers': {'answer_start': [114], 'text': ['در شرق شهر مبارکه']}}
